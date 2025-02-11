0ddad372fd3130a7bd49a882681e955cb445ce35
Wouter Vernaillen <wouter@vernaillen.com>
2025-02-11 06:32:35 +0100

    cleanup

--- playground/app/pages/index.vue
+++ playground/app/pages/index.vue
@@ -1,37 +1,11 @@
 <script setup lang="ts">
-import type { GeneralSettingsFragment, PostFragment } from '#build/graphql-operations'
-import { useHead, ref, computed } from '#imports'
-import { useGeneralSettings, usePosts, usePostsByCategoryName } from '#wpnuxt'
+import { useHead, useAsyncData } from '#imports'
+import { useGeneralSettings } from '#wpnuxt'
 
-/* const { data: posts } = await useAsyncData('posts', () => usePosts())
 const { data: settings } = await useAsyncData('settings', () => useGeneralSettings())
-const { data: latestPost } = await useAsyncData('latestPost', () => usePosts({ limit: 1 }))
-const { data: postsByCategory } = await useAsyncData('postsByCategory', () => usePostsByCategory({ categoryName: 'Lorem Ipsum' }))
-*/
-
-const isLoading = ref(true)
-const posts = ref<PostFragment[]>([])
-const settings = ref<GeneralSettingsFragment | null>(null)
-const latestPost = ref<PostFragment | null>(null)
-const postsByCategory = ref<PostFragment[]>([])
-
-async function fetch() {
-  isLoading.value = true
-  const { data: postsData } = await usePosts()
-  const { data: settingsData } = await useGeneralSettings()
-  const { data: latestPostData } = await usePosts({ limit: 1 })
-  const { data: postsByCategoryData } = await usePostsByCategoryName({ categoryName: 'Lorem Ipsum' })
-
-  posts.value = computed(() => postsData).value
-  settings.value = computed(() => settingsData).value
-  latestPost.value = computed(() => latestPostData?.[0] || null).value
-  postsByCategory.value = computed(() => postsByCategoryData).value
-  isLoading.value = false
-}
-fetch()
 
 useHead({
-  title: settings.value?.title
+  title: settings.value?.data?.title
 })
 </script>
 
